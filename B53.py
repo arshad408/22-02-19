a84a36bcce67abb14525e494eadc05e29f9153d3
arshad408 <arshadachu408@gmail.com>
2019-03-19 11:22:23 +0530

    Update B53.py

--- B53.py
+++ B53.py
@@ -1,7 +1,7 @@
-n=int(input("Enter a number:"))
+x=int(input("Enter a number:"))
 tot=0
-while(n>0):
-    dig=n%10
+while(x>0):
+    dig=x%10
     tot=tot+dig
-    n=n//10
+    x=x//10
 print("The total sum of digits is:",tot)
